a06d45397a8297694f132350d459c1f0b989b43f
Vikrant Mehta <vikrantmehta123@gmail.com>
2024-11-21 21:48:53 +0530

    Till 45 mins

--- bigram.py
+++ bigram.py
@@ -1,30 +1,47 @@
 import torch
 import matplotlib.pyplot as plt
 
-words =  open('names.txt', 'r').read().splitlines()
+words = open('names.txt', 'r').read().splitlines()
 chars = sorted(list(set(''.join(words))))
 
-stoi = {s:i+1 for i, s in enumerate(chars)}
+stoi = {s: i+1 for i, s in enumerate(chars)}
 stoi['.'] = 0
 
-itos = {i:s for s, i in stoi.items()}
+itos = {i: s for s, i in stoi.items()}
 
-N = torch.zeros((28, 28), dtype=torch.int32)
+N = torch.zeros((27, 27), dtype=torch.int32)
 
 for w in words:
-    chs = ["<S>"] + list(w) + ["<E>"]
-    for ch1, ch2 in zip(w, w[1:]):
+    chs = ["."] + list(w) + ["."]
+    for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
 
-plt.figure(figsize=(128, 128))
-plt.imshow(N, cmap="Blues")
+# plt.figure(figsize=(128,128))
+# plt.imshow(N, cmap='Blues')
+# for i in range(27):
+#     for j in range(27):
+#         chstr = itos[i] + itos[j]
+#         plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+#         plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
+# plt.axis('off')
+# plt.show()
 
-for i in range(27):
-    for j in range(27):
-        chstr = itos[i] + itos[j]
-        plt.text(j, i, chstr, ha='center', va='bottom', color='gray')
-        plt.text(j, i, N[i, j].item(), ha='center', va='top', color='gray')
-plt.axis('off')
-plt.show()
+P = (N).float()
+P = P / P.sum(dim=1, keepdim=True)
+
+g = torch.Generator().manual_seed(2147483647)
+
+idx = 0
+for i in range(5):
+    out = []
+    while True:
+        p = P[idx]
+
+        idx = torch.multinomial(
+            p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[idx])
+        if idx == 0:
+            break
+    print("".join(out))
